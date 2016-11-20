dbab7e15474649c9c845adee9ea74e9fb5a1d353
heim0040 <heim0040@hotmail.com>
2016-11-19 22:27:28 -0800

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -1,9 +1,10 @@
 library(dplyr)
 
+#read table
 df <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "NA", 
                  stringsAsFactors=F)
 
-
+#filter and transform data
 df %>% filter(as.Date(Date, format="%d/%m/%Y") >= as.Date("2007-02-01"),
               as.Date(Date, format="%d/%m/%Y") <= as.Date("2007-02-02")) %>% 
         mutate(Global_active_power = as.numeric(Global_active_power),
@@ -11,10 +12,9 @@ df %>% filter(as.Date(Date, format="%d/%m/%Y") >= as.Date("2007-02-01"),
 
 df2$datetime <- strptime(df2$datetime, format = "%d/%m/%Y %H:%M:%S")
 
-
-
-
+#create plot
 plot(df2$datetime, df2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
+#generate PNG version of plot
 dev.copy(png, 'Plot2.png')
 dev.off()    
--- Plot3.R
+++ Plot3.R
@@ -1,9 +1,10 @@
 library(dplyr)
 
+#read data
 df <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "NA", 
                  stringsAsFactors=F)
 
-
+#filter and transform data
 df %>% filter(as.Date(Date, format="%d/%m/%Y") >= as.Date("2007-02-01"),
               as.Date(Date, format="%d/%m/%Y") <= as.Date("2007-02-02")) %>% 
         mutate(Global_active_power = as.numeric(Global_active_power),
@@ -11,12 +12,13 @@ df %>% filter(as.Date(Date, format="%d/%m/%Y") >= as.Date("2007-02-01"),
 
 df2$datetime <- strptime(df2$datetime, format = "%d/%m/%Y %H:%M:%S")
 
+#create plot
 plot(df2$datetime, df2$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", main="")
 lines(df2$datetime, df2$Sub_metering_2, type="l", col="red")
 lines(df2$datetime, df2$Sub_metering_3, type="l", col="blue")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1),
        lwd=c(2.5,2.5,2.5), col=c("black","red","blue"))
 
-
+#generate PNG version of plot
 dev.copy(png, 'Plot3.png')
 dev.off()    
