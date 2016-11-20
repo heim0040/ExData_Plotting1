d197e1e3414371dee31e5a3c4825b9d78dd386fb
heim0040 <heim0040@hotmail.com>
2016-11-19 23:06:15 -0800

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -13,14 +13,16 @@ df2$datetime <- strptime(df2$datetime, format = "%d/%m/%Y %H:%M:%S")
 
 #create plots
 par(mfrow = c(2, 2))
+op <- par(cex=.64)
 plot(df2$datetime, df2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 plot(df2$datetime, df2$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(df2$datetime, df2$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", main="")
 lines(df2$datetime, df2$Sub_metering_2, type="l", col="red")
 lines(df2$datetime, df2$Sub_metering_3, type="l", col="blue")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1),
-       lwd=c(2.5,2.5,2.5), col=c("black","red","blue"))
+       lwd=c(2.5,2.5,2.5), col=c("black","red","blue"), bty="n")
 plot(df2$datetime, df2$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
 
 #generate PNG version of plots
 dev.copy(png, 'Plot4.png')
