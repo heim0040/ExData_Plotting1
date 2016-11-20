77079ced7762284eb697e319be9f53fec6b99381
heim0040 <heim0040@hotmail.com>
2016-11-19 23:43:25 -0800

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -12,8 +12,8 @@ df %>% filter(as.Date(Date, format="%d/%m/%Y") >= as.Date("2007-02-01"),
 df2$datetime <- strptime(df2$datetime, format = "%d/%m/%Y %H:%M:%S")
 
 #create plots and output as PNG
-png("Plot4.png",width=1100,height=900,bg="white")
-par(mar=c(4,4,2,2), mfrow = c(2, 2))
+png("Plot4.png",width=1920,height=1050,bg="white")
+par(mar=c(4,4,2,2), mfrow = c(2, 2), cex.lab=2, cex.axis=2, cex.main=2, cex.sub=2)
 plot(df2$datetime, df2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 plot(df2$datetime, df2$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(df2$datetime, df2$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", main="")
